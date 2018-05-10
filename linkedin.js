9baaa7a87476361289c9370d47f7b8db84dffe0e
Nicolas <nico@cooya.fr>
2018-05-10 15:06:22 +0200

    email gathering method implemented

--- linkedin.js
+++ linkedin.js
@@ -1,10 +1,13 @@
 const fs = require('fs');
+const url = require('url');
 const util = require('util');
 const readFile = util.promisify(fs.readFile);
 const writeFile = util.promisify(fs.writeFile);
 
 const csv = require('./csv.js');
 const puppeteer = require('puppeteer');
+const request = require('request');
+const get = util.promisify(request.get);
 
 const browserOptions = {
     args: [
@@ -31,18 +34,67 @@ const browserOptions = {
     const entries = (await csv.readFile(config.csvFile));
     console.log(entries.length + ' entries to process.');
 
-    // process entries
-    const browser = await puppeteer.launch(browserOptions);
-    await processEntries(browser, entries, config.csvFile, config.cookiesFile, config.screenshotFile);
-    await browser.close();
+    let endpoint;
+    let requestOptions;
+    let response;
+    let hostname;
+    let i = 0;
+    for(let entry of entries) {
+        if(entry.length != 9 || entry[8] == '') {
+            console.log('Skipping...');
+            continue;
+        }
+        hostname = url.parse(entry[8]).hostname.replace('www.', '');
+        entry[0] = entry[0].normalize('NFD').replace(/[\u0300-\u036f]/g, '');
+        entry[1] = entry[1].normalize('NFD').replace(/[\u0300-\u036f]/g, '');
+        endpoint = 'https://api.skrapp.io/api/v2/find?firstName=' + entry[0] + '&lastName=' + entry[1] + '&domain=' + hostname;
+        console.log(endpoint);
+        requestOptions = {
+            url: endpoint,
+            headers: {
+                'X-Access-Key': config.skrappApiKey,
+                'Content-Type': 'application/json'
+            }
+        };
 
-    // saving updated entries into csv file
-    await csv.writeFile(config.csvFile, entries);
+        try {
+            response = await get(requestOptions);
+            console.log(response.body);
+            response.body = JSON.parse(response.body);
+            if(response.body['email'])
+                entries[i] = entry.concat(response.body['email'], response.body['accuracy']);
+            else
+                entries[i] = entry.concat(['Email not found', '']);
+            console.log('Saving last processed entry...');
+            await csv.writeFile(config.csvFile, entries);
+            i++;
+        }
+        catch(e) {
+            console.log(e);
+            process.exit(1);
+        }
+    }
+
     console.log('Process done.');
 })();
 
-async function processEntries(browser, entries, csvFile, cookiesFile, screenshotFile) {
-    let page = null;
+/*(async () => {
+    const config = require('./assets/config.json');
+
+    // load entries from csv file
+    const entries = (await csv.readFile(config.csvFile));
+    console.log(entries.length + ' entries to process.');
+
+    // process entries
+    const browser = await puppeteer.launch(browserOptions);
+    const page = await createPage(browser, config.cookiesFile);
+    await processEntries(page, entries, config.csvFile);
+    await browser.close();
+
+    console.log('Process done.');
+})();*/
+
+async function processEntries(page, entries, csvFile) {
     let companyLink;
     let companyDetails;
     let entry;
@@ -50,24 +102,12 @@ async function processEntries(browser, entries, csvFile, cookiesFile, screenshot
         entry = entries[i];
         console.log('Processing entry', i, '...');
 
-        if(entry[5] || entry[6]) {
-            console.log('Skipping already processed entry.');
+        if(i == 0 || entry[6] == 'Unknown' || entry.length == 9) {
+            console.log('Skipping...');
             continue; // skip the already processed entries
         }
 
-        if(!page)
-            page = await createPage(browser, cookiesFile);
-
-        console.log('Going to %s...', entry[3]);
-        while(true) {
-            try {
-                await page.goto(entry[3]);
-                break;
-            }
-            catch(e) {
-                console.log(e);
-            }
-        }
+        await goTo(page, entry[3], 30000);
 
         console.log('Scrolling the page...');
         //await page.waitFor('span.org > a');
@@ -86,30 +126,26 @@ async function processEntries(browser, entries, csvFile, cookiesFile, screenshot
             }
         }
         // wait a bit to prevent robot detection
-        console.log('Waiting for 30000ms.');
-        await page.waitFor(30000);// 2 sec minimum required otherwise it does not work very well
+        console.log('Waiting for 15000ms.');
+        await page.waitFor(15000);// 2 sec minimum required otherwise it does not work very well
 
         // get the link of the company
         companyLink = await page.$('#experience-section > ul > li:nth-child(1) a.ember-view');
-        //companyLink = await page.$('span.org > a');
-        if (!companyLink)
+        if(!companyLink)
             throw Error('Cannot find the company link.');
 
         // going to the company page
-        const navigationPromise = page.waitForNavigation();
-        //await companyLink.click();
-        await page.evaluate((el) => {
-            el.click();
-        }, companyLink);
-        await navigationPromise;
+        await click(page, companyLink);
 
         // getting details about the company
-        if(page.url().indexOf('https://www.linkedin.com/search/results/index/') !== -1)
-            entries[i][5] = 'No company URL available';
+        const companyPageUrl = page.url();
+        if(companyPageUrl.indexOf('https://www.linkedin.com/search/results/index/') != -1)
+            throw Error('Impossible error !');
         else {
             companyDetails = await scrapCompanyPage(page);
-            entries[i][5] = companyDetails['headquarters'] || 'Unknown';
-            entries[i][6] = companyDetails['companySize'] || 'Unknown';
+            //entries[i][5] = companyDetails['headquarters'] || 'Unknown';
+            //entries[i][6] = companyDetails['companySize'] || 'Unknown';
+            entries[i] = entry.concat([companyPageUrl, companyDetails['website']]);
         }
 
         // saving the retrieved details in the csv file
@@ -142,6 +178,48 @@ async function createPage(browser, cookiesFile) {
     await page.setUserAgent('Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:57.0) Gecko/20100101 Firefox/57.0');
     await loadCookies(page, cookiesFile);
     return page;
+}
+
+async function goTo(page, url, timeout) {
+    console.log('Going to %s...', url);
+
+    const options = timeout ? {timeout: timeout} : {};
+    let again = true;
+    while(again) {
+        try {
+            await page.goto(url, options);
+            again = false;
+        }
+        catch(e) {
+            if(e.message.indexOf('Navigation Timeout Exceeded') != -1)
+                console.log('goTo() timeout !');
+            else
+                throw e;
+        }
+    }
+}
+
+async function click(page, element, timeout) {
+    console.log('Clicking on element...');
+
+    const options = timeout ? {timeout: timeout} : {};
+    let again = true;
+    while(again) {
+        try {
+            const navigationPromise = page.waitForNavigation(options);
+            await page.evaluate((el) => {
+                el.click();
+            }, element);
+            await navigationPromise;
+            again = false;
+        }
+        catch(e) {
+            if(e.message.indexOf('Navigation Timeout Exceeded') != -1)
+                console.log('click() timeout !');
+            else
+                throw e;
+        }
+    }
 }
 
 async function logIn(page, login, password) {
