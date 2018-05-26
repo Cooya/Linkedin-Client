1e4a76a734b797c801be23f9ae25edf7c60cebda
Nicolas <nico@cooya.fr>
2018-05-26 11:50:35 +0200

    company schema improved

--- server/models.js
+++ server/models.js
@@ -32,7 +32,7 @@ const companySchema = new mongoose.Schema({
     },
     website: {
         type: String,
-        required: true
+        required: false
     },
     headquarters: {
         type: String,
